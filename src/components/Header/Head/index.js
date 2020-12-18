c9655e6ac8c31ff59357c79622cb0d0e31e1f332
Elvis Saavedra <elvis.saavedra.german@gmail.com>
2020-12-17 20:56:39 -0500

    First test

--- src/components/Header/Head/index.js
+++ src/components/Header/Head/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
 export const Head = () => {
 	return (
-		<div className='head-div'>
-			<h1>La Republica</h1>
-		</div>
+		<Link to={'/'}>
+			<div className='head-div'>
+				<h1>La Republica</h1>
+			</div>
+		</Link>
 	);
 };
